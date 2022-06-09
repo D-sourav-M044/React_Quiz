ad46f7bf83d62af48758475c42e1a2b1e9c8209e
D-sourav-M044 <45461265+D-sourav-M044@users.noreply.github.com>
2022-06-09 13:48:49 +0600

    html

---
+++ src/components/Nav.js
@@ -0,0 +1,26 @@
+import logo from "../assets/images/logo-bg.png";
+import classes from "../styles/Nav.modules.css";
+
+export default function Nav() {
+  return (
+    <div>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <a href="index.html" class={classes.brand}>
+              <img src={logo} alt="Learn with Sumit Logo" />
+              <h3>Learn with Sumit</h3>
+            </a>
+          </li>
+        </ul>
+        {/* <div class="account">
+        <span class="material-icons-outlined" title="Account">
+          account_circle
+        </span>
+        <a href="signup.html">Signup</a>
+        <!-- <span class="material-icons-outlined" title="Logout"> logout </span> -->
+      </div> */}
+      </nav>
+    </div>
+  );
+}
